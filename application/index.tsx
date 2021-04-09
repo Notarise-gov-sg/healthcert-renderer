50292ba2f37f53bce904e43376d99aefc5072376
yaojie <limyaojie93@gmail.com>
2021-04-09 12:08:32 +0800

    Update index.tsx to test multiple memo page splitting

--- application/index.tsx
+++ application/index.tsx
@@ -1,23 +1,14 @@
 import ReactDOM from "react-dom";
-import { customTemplateCertificate } from "../src/templates/samples";
+import { multiMemoSample } from "../src/templates/healthcert/fixtures/multiMemo";
+import { healthCertSample } from "../src/templates/healthcert/fixtures/sampleWithNric";
 import React from "react";
 import { App } from "./app";
 
 ReactDOM.render(
   <App
     documents={[
-      { name: "Default document", document: customTemplateCertificate },
-      {
-        name: "Red document",
-        document: {
-          ...customTemplateCertificate,
-          foo: "bar",
-          $template: {
-            ...customTemplateCertificate.$template,
-            name: "red"
-          }
-        }
-      }
+      { name: "Multi Memo document", document: multiMemoSample },
+      { name: "Single Memo document", document: healthCertSample }
     ]}
   />,
   document.getElementById("root")
