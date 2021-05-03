485d3506a5a482985caceda0ebe63547946fe0bf
eugbyte <eugenetham1994@gmail.com>
2021-05-03 16:41:07 +0800

    chore: allow access origin from  verify.gov.sg for iframe access

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { FramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
 import { registry } from "./templates";
+import { Helmet } from "react-helmet";
 
-ReactDOM.render(<FramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
+const Root: React.FC = () => (
+  <>
+    <Helmet>
+      {/* To allow verify.gov.sg to access the dom rendered through iframe */}
+      <meta httpEquiv="Access-Control-Allow-Origin" content="https://www.verify.gov.sg/" />
+    </Helmet>
+    <FramedDocumentRenderer templateRegistry={registry} />
+  </>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
