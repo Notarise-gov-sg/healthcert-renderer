067bc683d907fe8affa4dde44b98afb8da919b34
eugbyte <eugenetham1994@gmail.com>
2021-05-03 17:31:36 +0800

    chore: removed react helmet / headers as it is not worrking. healthcerrt renderer is not a server

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { FramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
 import { registry } from "./templates";
-import { Helmet } from "react-helmet";
 
-const Root: React.FC = () => (
-  <>
-    <Helmet>
-      {/* To allow verify.gov.sg to access the dom rendered through iframe */}
-      <meta httpEquiv="Access-Control-Allow-Origin" content="http://localhost:3000/" />
-    </Helmet>
-    <FramedDocumentRenderer templateRegistry={registry} />
-  </>
-);
-
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<FramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
