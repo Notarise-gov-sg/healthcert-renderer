80fedd38f5e37e430a15e9d44f110f2253ac3722
eugbyte <eugenetham1994@gmail.com>
2021-05-03 17:34:37 +0800

    chore: installed forked decentralised renderer. It works now

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import { FramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
 import { registry } from "./templates";
 
+// installed the forked decentralised renderer instead
 ReactDOM.render(<FramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
