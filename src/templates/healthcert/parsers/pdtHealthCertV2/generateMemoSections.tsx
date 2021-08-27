be73225f38dfec6b43978b6c6ec86424fd659858
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-08-27 09:55:32 +0800

    fix: rendered date formats

--- src/templates/healthcert/parsers/pdtHealthCertV2/generateMemoSections.tsx
+++ src/templates/healthcert/parsers/pdtHealthCertV2/generateMemoSections.tsx
@@ -4,6 +4,7 @@ import { R4 } from "@ahryman40k/ts-fhir-types";
 import { NotarisedPDTHealthCertUnwrappedV2 } from "../../types";
 import fhirHelper from "../../../../models/fhirHelper";
 import { MemoSection } from "../../memo/pdtV2Memo";
+import { isoToDateOnlyString, isoToLocaleString } from "../../../../util/datetime";
 
 export const generateMemoSections = (document: NotarisedPDTHealthCertUnwrappedV2): JSX.Element[] => {
   const { patient, observations } = fhirHelper.parse(document.fhirBundle as R4.IBundle);
@@ -15,12 +16,12 @@ export const generateMemoSections = (document: NotarisedPDTHealthCertUnwrappedV2
       patientNricFin={patient.nricFin}
       patientPassportNumber={patient.passportNumber}
       patientNationalityCode={String(patient.nationality.code)}
-      patientBirthDate={patient.birthDate}
+      patientBirthDate={isoToDateOnlyString(patient.birthDate)}
       observationTestTypeDisplay={observation.testType.display || ""}
-      observationEffectiveDateTime={observation.effectiveDateTime}
+      observationEffectiveDateTime={isoToLocaleString(observation.effectiveDateTime)}
       observationResultDisplay={observation.result.display || ""}
       specimenSwabTypeDisplay={specimen.swabType.display || ""}
-      specimenCollectionDateTime={specimen.collectionDateTime}
+      specimenCollectionDateTime={isoToLocaleString(specimen.collectionDateTime)}
       organizationLhpName={organization.lhp.fullName}
       organizationAlName={organization.al?.fullName}
       practitionerName={practitioner.fullName}
