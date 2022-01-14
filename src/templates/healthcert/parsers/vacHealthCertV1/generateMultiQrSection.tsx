7a03cca1c31c98fc16367f1281c04a67ced032c0
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2022-01-14 17:57:30 +0800

    Feat: 180884785 Dose numbering for Offline QR (#128)
    * Feat: 180884785 Dose numbering for Offline QR
    
    * Update vaccSampleMultiDoseMultiQr.ts

--- src/templates/healthcert/parsers/vacHealthCertV1/generateMultiQrSection.tsx
+++ src/templates/healthcert/parsers/vacHealthCertV1/generateMultiQrSection.tsx
@@ -1,76 +1,54 @@
 import React from "react";
 import QRCode from "qrcode.react";
-import { groupBy } from "lodash";
-import { notarise } from "@govtechsg/oa-schemata";
-import { Immunization, NotarisedHealthCert } from "../../types";
+import { get } from "lodash";
+import { NotarisedHealthCert } from "../../types";
 import {
   Bold,
   QrCodeContainerWithBorder,
   ResultSection,
   QrRowCenter,
   QrColCenter,
-  QrTitle,
   StyledMemoSection,
 } from "../../styled-components";
 import EULogo from "../../eu-dcc-tag-big.png";
+import sddInformations from "../../../../../static/SDD.mapping.json";
 
-interface GroupedOfflineQr {
-  manufacturer: string;
-  signedEuHealthCerts: notarise.SignedEuHealthCert[];
-}
-
-const OfflineQrGroup: React.FC<{
-  groupedOfflineQr: GroupedOfflineQr;
-}> = ({ groupedOfflineQr }) => {
+const generateOfflineQrSection = (
+  document: NotarisedHealthCert
+): JSX.Element => {
+  const signedEuHealthCerts =
+    document.notarisationMetadata.signedEuHealthCerts || [];
   const logoWidth = 130;
   const logoHeight = logoWidth / 5;
   return (
     <StyledMemoSection>
-      <QrTitle>{groupedOfflineQr.manufacturer}</QrTitle>
       <QrRowCenter>
-        {groupedOfflineQr.signedEuHealthCerts.map((signedEuHealthCert, i) => (
-          <QrColCenter key={i}>
-            <QrCodeContainerWithBorder key={i}>
-              <QRCode
-                value={signedEuHealthCert.qr}
-                level={"M"}
-                size={200}
-                imageSettings={{
-                  src: EULogo,
-                  width: logoWidth,
-                  height: logoHeight,
-                }}
-              />
-            </QrCodeContainerWithBorder>
-            DOSE {signedEuHealthCert.dose}
-          </QrColCenter>
-        ))}
+        {signedEuHealthCerts.map((signedEuHealthCert, i) => {
+          const sddInfo = signedEuHealthCert.vaccineCode
+            ? get(sddInformations, signedEuHealthCert.vaccineCode)
+            : { short_name: "" };
+          return (
+            <QrColCenter key={i}>
+              <QrCodeContainerWithBorder key={i}>
+                <QRCode
+                  value={signedEuHealthCert.qr}
+                  level={"M"}
+                  size={200}
+                  imageSettings={{
+                    src: EULogo,
+                    width: logoWidth,
+                    height: logoHeight,
+                  }}
+                />
+              </QrCodeContainerWithBorder>
+              {sddInfo.short_name} (DOSE {signedEuHealthCert.dose})
+            </QrColCenter>
+          );
+        })}
       </QrRowCenter>
       <br />
     </StyledMemoSection>
   );
-};
-
-const generateOfflineQrSection = (
-  document: NotarisedHealthCert
-): JSX.Element[] => {
-  const signedEuHealthCerts =
-    document.notarisationMetadata.signedEuHealthCerts || [];
-  const groupedEuHealthCerts = groupBy(signedEuHealthCerts, "vaccineCode");
-  const groupedEuHealthCertKeys = Object.keys(groupedEuHealthCerts);
-
-  return groupedEuHealthCertKeys.map((vaccineCode, i) => {
-    const immunization = document.fhirBundle.entry.find(
-      (entry) =>
-        entry.resourceType === "Immunization" &&
-        entry.vaccineCode.coding[0]?.code === vaccineCode
-    ) as Immunization;
-    const groupedOfflineQr = {
-      manufacturer: immunization?.vaccineCode?.coding[0]?.display || "",
-      signedEuHealthCerts: groupedEuHealthCerts[vaccineCode],
-    };
-    return <OfflineQrGroup key={i} groupedOfflineQr={groupedOfflineQr} />;
-  });
 };
 
 export const generateMultiQrSection = (
