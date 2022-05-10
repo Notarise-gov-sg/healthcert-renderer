8524e051ae7d525cb9992705ec778a48ccb8a62f
Alvin Leu <witxhhaven@gmail.com>
2022-05-10 10:50:19 +0800

    added css to prevent qrcode from cutting

--- src/components/qrcode.tsx
+++ src/components/qrcode.tsx
@@ -26,7 +26,10 @@ export const QrCode = ({
 
   return (
     <canvas
-      style={hasBorder ? { border: "1px solid #e2e8f0" } : {}}
+      style={{
+        border: hasBorder ? "1px solid #e2e8f0" : "none",
+        pageBreakInside: "avoid",
+      }}
       ref={canvasRef}
     />
   );
