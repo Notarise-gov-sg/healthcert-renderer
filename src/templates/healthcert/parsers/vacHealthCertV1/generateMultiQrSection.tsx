82e5ba4aa1cfdd7788180d07458e60ba0d39f802
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-10-18 12:05:34 +0800

    fix: lint warning

--- src/templates/healthcert/parsers/vacHealthCertV1/generateMultiQrSection.tsx
+++ src/templates/healthcert/parsers/vacHealthCertV1/generateMultiQrSection.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { QrCode } from "../../../../components/qrcode";
-import { get } from "lodash";
 import { NotarisedHealthCert } from "../../types";
 import {
   Bold,
